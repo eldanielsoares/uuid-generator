b65a478808f559eda710302bb4dd34187d10c216
eldanielsoares <danielsoares7717@gmail.com>
2024-12-03 19:58:07 -0300

    fix: script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import './globals.css';
 import Footer from '@/components/Footer';
 import { Suspense } from 'react';
+import Script from 'next/script';
 
 
 const geistSans = localFont({
@@ -31,6 +32,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+        async
+        src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8663662446032541'
+        crossOrigin="anonymous"
+        />
         <NuqsAdapter>
         {children}
         </NuqsAdapter>
